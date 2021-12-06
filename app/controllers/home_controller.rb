857e2c665d64cc2bf096c691a6e23807a0ef5cf0
Filipeem <filipe.m@outlook.com>
2021-12-06 09:27:17 -0300

    busca por tarefas pendentes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,12 @@ class HomeController < ApplicationController
   def welcome
     @tasks = Task.all
   end
+
   def search
     @tasks = Task.where('description like ?', "%#{params[:q]}%")
+  end
+
+  def undone
+    @tasks = Task.where('status = false')
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'home#welcome'
   get 'search', to:"home#search"
+  get 'undone', to:"home#undone"
   resources :tasks
 end
